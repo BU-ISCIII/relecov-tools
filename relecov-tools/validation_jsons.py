f40d441e34fa9c038480ba25568a93296ce455ee
luissian <chapado.l@gmail.com>
2022-01-31 17:42:56 +0100

    test validation

--- relecov-tools/validation_jsons.py
+++ relecov-tools/validation_jsons.py
@@ -2,32 +2,33 @@
 from jsonschema import validate
 from jsonschema import Draft202012Validator
 import json
-import pandas as pd
+#import pandas as pd
+import excel2json-3
 
 class RelecovSchema :
     def __init__ (self, schema):
         self.schema = json.load(schema)
-        
+
     def get_gontology(self,geontology):
         for key, value in schema['properties'].items():
             if value['ontology'] == geontology :
                 return value
         return None
-        
+
 
 class PhagePlus :
     def __init__ (self,data, json_schema):
         self.data = data
         self.schema = json_schema
-        
-        
+
+
     def convert_json(schema):
         pass
-    
-    def get_data(self, field):
-        return self.data[field])
 
-data = {"sample_name":'s1', "collecting_institution" :'inst2',
+    def get_data(self, field):
+        return self.data[field]
+
+data ={"sample_name":'s1', "collecting_institution" :'inst2',
     "submitting_institution":'sub',
     "sample_collection_date": '12/02/2022',
     "geo_loc_country":'Afghanistan',
@@ -41,6 +42,34 @@ data = {"sample_name":'s1', "collecting_institution" :'inst2',
     "consensus_sequence_software_name":'MinIon',
     "consensus_sequence_software_version":'Ivar'
 }
+data2 = [{"sample_name":'s1', "collecting_institution" :'inst2',
+    "submitting_institution":'sub',
+    "sample_collection_date": '12/02/2022',
+    "geo_loc_country":'Afghanistan',
+    "geo_loc_state":'Western',
+    "organism":'Coronaviridae',
+    "isolate":'SARS-CoV-2/',
+    "host_scientific_name":'Bos taurus',
+    "host_disease":'Homo sapiens',
+    "sequencing_instrument_model" :'COVID-19',
+    "sequencing_instrument_platform": 'Illumina sequencing instrument',
+    "consensus_sequence_software_name":'MinIon',
+    "consensus_sequence_software_version":'Ivar'
+},
+{"sample_name":'s1', "collecting_institution" :'inst3',
+    "submitting_institution":'sub',
+    "sample_collection_date": '12/02/2022',
+    "geo_loc_country":'Afghanistan',
+    "geo_loc_state":'Western',
+    "organism":'Coronaviridae',
+    "isolate":'SARS-CoV-2/',
+    "host_scientific_name":'Bos taurus',
+    "host_disease":'Homo sapiens',
+    "sequencing_instrument_model" :'COVID-19',
+    "sequencing_instrument_platform": 'Illumina sequencing instrument',
+    "consensus_sequence_software_name":'MinIon',
+    "consensus_sequence_software_version":'Ivar'
+}]
 
 schema_file = open('/home/lchapado/Projects/Proyecto_ERA/relecov-tools/schema/phage_plus_V0.json')
 
@@ -56,11 +85,11 @@ try:
 except:
     print('Invalid input data')
     exit(1)
-
+import pdb; pdb.set_trace()
 sample_list = []
 df = pd.read_excel('sample.xlsx')
 for idx, row in df.iterrows():
     sample_list.append(PhagePlus(row.to_dict(),phage_plus_schema))
-   
+
 
 print('Completed')
