c3a31c897302487112722a4c9543a8abaef1f1c5
luissian <chapado.l@gmail.com>
2022-02-02 17:01:36 +0100

    testing the excel validation

--- relecov-tools/validation_jsons.py
+++ relecov-tools/validation_jsons.py
@@ -3,20 +3,34 @@ from jsonschema import validate
 from jsonschema import Draft202012Validator
 import json
 #import pandas as pd
-import excel2json-3
+#import excel2json-3
 
-class RelecovSchema :
+class PhagePlusSchema :
     def __init__ (self, schema):
-        self.schema = json.load(schema)
+        self.schema = schema
 
-    def get_gontology(self,geontology):
-        for key, value in schema['properties'].items():
-            if value['ontology'] == geontology :
-                return value
-        return None
+    def get_gontology(self,property_item):
+        '''
+        Description:
+            The function return the geontology value for a property in the schema
+        Input:
+            property_item    # property name to fetch its geontology
+        Return:
+            Return ontology value or None
+        '''
+        try:
+             return self.schema['properties'][property_item]['ontology']
+        except:
+            return None
+
+    def maping_schemas_based_on_geontology(mappep_to_schema):
+        '''
+
+        '''
+        return
 
 
-class PhagePlus :
+class PhagePlusData :
     def __init__ (self,data, json_schema):
         self.data = data
         self.schema = json_schema
@@ -71,7 +85,9 @@ data2 = [{"sample_name":'s1', "collecting_institution" :'inst2',
     "consensus_sequence_software_version":'Ivar'
 }]
 
-schema_file = open('/home/lchapado/Projects/Proyecto_ERA/relecov-tools/schema/phage_plus_V0.json')
+# schema_file = open('/home/lchapado/Projects/Proyecto_ERA/relecov-tools/schema/phage_plus_V0.json')
+schema_file = open('/home/bioinfo/Projects/relecov-tools/schema/phage_plus_V0.json')
+
 
 json_phage_plus_schema = json.load(schema_file)
 try:
@@ -79,29 +95,39 @@ try:
 except:
     print('Invalid schema')
     exit(1)
-phage_plus_schema = RelecovSchema(json_phage_plus_schema)
-try:
-    validate(instance=data,schema=json_phage_plus_schema)
-except:
-    print('Invalid input data')
-    exit(1)
-import pdb; pdb.set_trace()
+phage_plus_schema = PhagePlusSchema(json_phage_plus_schema)
+#try:
+#    validate(instance=data,schema=json_phage_plus_schema)
+#except:
+#    print('Invalid input data')
+#    exit(1)
+
 sample_list = []
-df = pd.read_excel('sample.xlsx')
-for idx, row in df.iterrows():
-    sample_list.append(PhagePlus(row.to_dict(),phage_plus_schema))
+#df = pd.read_excel('sample.xlsx')
+#for idx, row in df.iterrows():
+#    sample_list.append(PhagePlus(row.to_dict(),phage_plus_schema))
 from openpyxl import Workbook
-wbFile = openpyxl.load_workbook('my_test_file.xlsx', data_only=True)
-wsFile = wbFile['METADATA_LAB']
+import openpyxl
+from itertools import islice
+excel_input_file = '/home/bioinfo/Projects/relecov-tools/test/my_test_file.xlsx'
+wb_file = openpyxl.load_workbook(excel_input_file, data_only=True)
+ws_metadata_lab = wb_file['METADATA_LAB']
 heading = []
-for cell in wsFile[1]:
+for cell in ws_metadata_lab[1]:
     heading.append(cell.value)
 
-for row in islice(wsFile.values,1,wsFile.max_row):
-    samples = OrderedDict()
+for row in islice(ws_metadata_lab.values,1,ws_metadata_lab.max_row):
+    sample_data_row = {}
     for idx in range(len(heading)):
-        samples[heading[idx]] = row[idx].value
-    
+        sample_data_row[heading[idx]] = row[idx]
 
+    try:
+        import pdb; pdb.set_trace()
+        validate(instance=json.dumps(sample_data_row),schema=json_phage_plus_schema)
+    except:
+        print('Unsuccessful validation for sample ' , sample_data_row['sample_name'])
+        continue
 
+        sample_list.append(PhagePlusData(sample_data_row,phage_plus_schema))
+import pdb; pdb.set_trace()
 print('Completed')
