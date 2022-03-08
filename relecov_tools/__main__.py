68fddd165796c9653764a415951014b2d4f613e1
luissian <chapado.l@gmail.com>
2022-03-08 13:06:16 +0100

    Changed the heading to RELECOV

--- relecov_tools/__main__.py
+++ relecov_tools/__main__.py
@@ -7,40 +7,50 @@ import click
 import rich.console
 import rich.logging
 import rich.traceback
-import sys
 import os
 import utils
-#import nf_core
-# conda install -c conda-forge click
-# conda install -c conda-forge rich
+
+log = logging.getLogger()
+
 
 def run_bu_isciii():
-      # Set up rich stderr console
+    # Set up rich stderr console
     stderr = rich.console.Console(stderr=True, force_terminal=utils.rich_force_colors())
 
     # Set up the rich traceback
     rich.traceback.install(console=stderr, width=200, word_wrap=True, extra_lines=1)
 
     # Print nf-core header
-    #stderr.print("\n[green]{},--.[grey39]/[green],-.".format(" " * 42), highlight=False)
-    stderr.print("[blue]                 ___              ___    ___   ___  ___   ___   ____   ", highlight=False)
-    stderr.print("[blue]   \    |-[grey39]-|  [blue]  |   \   |   |      |    |     |      |     |      |    ", highlight=False)
-    stderr.print("[blue]    \   \  [grey39]/ [blue]   |__ /   |   | ___  |    |__   |      |     |      |    ", highlight=False)
-    stderr.print("[blue]    /  [grey39] / [blue] \    |   \   |   |      |       |  |      |     |      |    ", highlight=False)
-    stderr.print("[blue]   /   [grey39] |-[blue]-|    |__ /   |___|     _|__  ___|  |___  _|_   _|_    _|_   ", highlight=False)
+    # stderr.print("\n[green]{},--.[grey39]/[green],-.".format(" " * 42), highlight=False)
+    stderr.print(
+        "[blue]                ___   ___       ___  ___  ___                           ",
+        highlight=False,
+    )
+    stderr.print(
+        "[blue]   \    |-[grey39]-|  [blue] |   \ |    |    |    |    |   | \      /  ",
+        highlight=False,
+    )
+    stderr.print(
+        "[blue]    \   \  [grey39]/ [blue]  |__ / |__  |    |___ |    |   |  \    /   ",
+        highlight=False,
+    )
+    stderr.print(
+        "[blue]    /  [grey39] / [blue] \   |  \  |    |    |    |    |   |   \  /    ",
+        highlight=False,
+    )
+    stderr.print(
+        "[blue]   /   [grey39] |-[blue]-|   |   \ |___ |___ |___ |___ |___|    \/     ",
+        highlight=False,
+    )
 
-
-    #stderr.print("[green]                                          `._,._,'\n", highlight=False)
-    __version__ = '0.0.1'
-    stderr.print("[grey39]    BU-ISCIII-tools version {}".format(__version__), highlight=False)
-    try:
-        pass
-    except:
-        pass
+    # stderr.print("[green]                                          `._,._,'\n", highlight=False)
+    __version__ = "0.0.1"
+    stderr.print(
+        "[grey39]    RELECOV-tools version {}".format(__version__), highlight=False
+    )
 
     # Lanch the click cli
     bu_isciii_cli()
-
 
 
 # Customise the order of subcommands for --help
@@ -56,7 +66,13 @@ class CustomHelpOrder(click.Group):
     def list_commands_for_help(self, ctx):
         """reorder the list of commands when listing the help"""
         commands = super(CustomHelpOrder, self).list_commands(ctx)
-        return (c[1] for c in sorted((self.help_priorities.get(command, 1000), command) for command in commands))
+        return (
+            c[1]
+            for c in sorted(
+                (self.help_priorities.get(command, 1000), command)
+                for command in commands
+            )
+        )
 
     def command(self, *args, **kwargs):
         """Behaves the same as `click.Group.command()` except capture
@@ -74,19 +90,28 @@ class CustomHelpOrder(click.Group):
 
 
 @click.group(cls=CustomHelpOrder)
-#@click.version_option(nf_core.__version__)
-@click.option("-v", "--verbose", is_flag=True, default=False, help="Print verbose output to the console.")
-@click.option("-l", "--log-file", help="Save a verbose log to a file.", metavar="<filename>")
+@click.option(
+    "-v",
+    "--verbose",
+    is_flag=True,
+    default=False,
+    help="Print verbose output to the console.",
+)
+@click.option(
+    "-l", "--log-file", help="Save a verbose log to a file.", metavar="<filename>"
+)
 def bu_isciii_cli(verbose, log_file):
 
     # Set the base logger to output DEBUG
     log.setLevel(logging.DEBUG)
 
-     # Set up logs to the console
+    # Set up logs to the console
     log.addHandler(
         rich.logging.RichHandler(
             level=logging.DEBUG if verbose else logging.INFO,
-            console=rich.console.Console(stderr=True, force_terminal=nf_core.utils.rich_force_colors()),
+            console=rich.console.Console(
+                stderr=True, force_terminal=nf_core.utils.rich_force_colors()
+            ),
             show_time=False,
             markup=True,
         )
@@ -96,8 +121,13 @@ def bu_isciii_cli(verbose, log_file):
     if log_file:
         log_fh = logging.FileHandler(log_file, encoding="utf-8")
         log_fh.setLevel(logging.DEBUG)
-        log_fh.setFormatter(logging.Formatter("[%(asctime)s] %(name)-20s [%(levelname)-7s]  %(message)s"))
+        log_fh.setFormatter(
+            logging.Formatter(
+                "[%(asctime)s] %(name)-20s [%(levelname)-7s]  %(message)s"
+            )
+        )
         log.addHandler(log_fh)
+
 
 # pipeline list
 @bu_isciii_cli.command(help_priority=1)
@@ -110,7 +140,9 @@ def bu_isciii_cli(verbose, log_file):
     help="How to sort listed pipelines",
 )
 @click.option("--json", is_flag=True, default=False, help="Print full output as JSON")
-@click.option("--show-archived", is_flag=True, default=False, help="Print archived workflows")
+@click.option(
+    "--show-archived", is_flag=True, default=False, help="Print archived workflows"
+)
 def list(keywords, sort, json, show_archived):
     """
     List available bu-isciii workflows used for relecov.
@@ -123,10 +155,16 @@ def list(keywords, sort, json, show_archived):
 # sftp
 @bu_isciii_cli.command(help_priority=2)
 @click.argument("pipeline", required=False, metavar="<pipeline name>")
-@click.option("-r", "--revision", help="Release/branch/SHA of the project to run (if remote)")
+@click.option(
+    "-r", "--revision", help="Release/branch/SHA of the project to run (if remote)"
+)
 @click.option("-i", "--id", help="ID for web-gui launch parameter set")
 @click.option(
-    "-c", "--command-only", is_flag=True, default=False, help="Create Nextflow command with params (no params file)"
+    "-c",
+    "--command-only",
+    is_flag=True,
+    default=False,
+    help="Create Nextflow command with params (no params file)",
 )
 @click.option(
     "-o",
@@ -135,20 +173,37 @@ def list(keywords, sort, json, show_archived):
     default=os.path.join(os.getcwd(), "nf-params.json"),
     help="Path to save run parameters file",
 )
-def sftp(pipeline, id, revision, command_only, params_in, params_out, save_all, show_hidden, url):
-    '''
+def sftp(
+    pipeline,
+    id,
+    revision,
+    command_only,
+    params_in,
+    params_out,
+    save_all,
+    show_hidden,
+    url,
+):
+    """
     Download files located in sftp server.
 
-    '''
+    """
     print(nf_core.list.list_workflows(keywords, sort, json, show_archived))
+
 
 # metadata
 @bu_isciii_cli.command(help_priority=3)
 @click.argument("pipeline", required=False, metavar="<pipeline name>")
-@click.option("-r", "--revision", help="Release/branch/SHA of the project to run (if remote)")
+@click.option(
+    "-r", "--revision", help="Release/branch/SHA of the project to run (if remote)"
+)
 @click.option("-i", "--id", help="ID for web-gui launch parameter set")
 @click.option(
-    "-c", "--command-only", is_flag=True, default=False, help="Create Nextflow command with params (no params file)"
+    "-c",
+    "--command-only",
+    is_flag=True,
+    default=False,
+    help="Create Nextflow command with params (no params file)",
 )
 @click.option(
     "-o",
@@ -157,12 +212,23 @@ def sftp(pipeline, id, revision, command_only, params_in, params_out, save_all, 
     default=os.path.join(os.getcwd(), "nf-params.json"),
     help="Path to save run parameters file",
 )
-def metadata(pipeline, id, revision, command_only, params_in, params_out, save_all, show_hidden, url):
-    '''
+def metadata(
+    pipeline,
+    id,
+    revision,
+    command_only,
+    params_in,
+    params_out,
+    save_all,
+    show_hidden,
+    url,
+):
+    """
     Read Metadata .
 
-    '''
+    """
     print(nf_core.list.list_workflows(keywords, sort, json, show_archived))
-    
+
+
 if __name__ == "__main__":
     run_bu_isciii()
