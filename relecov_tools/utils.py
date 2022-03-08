ead9507d5e812a59f46c3595ed80e5ea2e28b2b9
luissian <chapado.l@gmail.com>
2022-03-08 15:31:47 +0100

    include functions for rich console

--- relecov_tools/utils.py
+++ relecov_tools/utils.py
@@ -1,4 +1,10 @@
+#!/usr/bin/env python
+"""
+Common utility function for bu-isciii package.
+"""
 import os
+import rich
+import questionary
 
 
 def file_exists(file_to_check):
@@ -24,3 +30,73 @@ def rich_force_colors():
     ):
         return True
     return None
+
+
+stderr = rich.console.Console(
+    stderr=True, style="dim", highlight=False, force_terminal=rich_force_colors()
+)
+
+
+def prompt_resolution_id():
+    stderr.print(
+        "Specify the name resolution id for the service you want to create. You can obtain this from iSkyLIMS. eg. SRVCNM584.1"
+    )
+    resolution_id = questionary.text("Resolution id").unsafe_ask()
+    return resolution_id
+
+
+def prompt_service_dir_path():
+    stderr.print("Service path to copy to execution temporal directory")
+    source = questionary.path("Source path").unsafe_ask()
+    return source
+
+
+def prompt_tmp_dir_path():
+    stderr.print("Temporal directory destination to execute sercive")
+    source = questionary.path("Source path").unsafe_ask()
+    return source
+
+
+def prompt_source_path():
+    stderr.print("Directory containing files cd to transfer")
+    source = questionary.path("Source path").unsafe_ask()
+    return source
+
+
+def prompt_destination_path():
+    stderr.print("Directory to which the files will be transfered")
+    destination = questionary.path("Destination path").unsafe_ask()
+    return destination
+
+
+def prompt_selection(msg, choices):
+    selection = questionary.select(msg, choices=choices).unsafe_ask()
+    return selection
+
+
+def prompt_path(msg):
+    source = questionary.path(msg).unsafe_ask()
+    return source
+
+
+def prompt_yn_question(msg):
+    confirmation = questionary.confirm(msg).unsafe_ask()
+    return confirmation
+
+
+def prompt_skip_folder_creation():
+    stderr.print("Do you want to skip folder creation? (Y/N)")
+    confirmation = questionary.confirm("Skip?", default=False).unsafe_ask()
+    return confirmation
+
+
+def get_service_ids(services_requested):
+    service_id_list = []
+    for services in services_requested:
+        service_id_list.append(services["serviceId"])
+    service_id_list.append("all")
+    stderr.print("Which selected service do you want to manage?")
+    services_sel = [prompt_selection("Service label:", service_id_list)]
+    if services_sel == "all":
+        services_sel == service_id_list
+    return services_sel
