7c493c80a72f0609ac9411e3a2f316ec9afa4b8b
luissian <chapado.l@gmail.com>
2022-03-08 15:07:43 +0100

    Fixing liting issues

--- relecov_tools/utils.py
+++ relecov_tools/utils.py
@@ -1,12 +1,13 @@
 import os
 
+
 def file_exists(file_to_check):
-    '''
+    """
     Input:
         file_to_check   # file name to check if exists
     Return:
         True if exists
-    '''
+    """
     if os.path.isfile(file_to_check):
         return True
     return False
@@ -16,6 +17,10 @@ def rich_force_colors():
     """
     Check if any environment variables are set to force Rich to use coloured output
     """
-    if os.getenv("GITHUB_ACTIONS") or os.getenv("FORCE_COLOR") or os.getenv("PY_COLORS"):
+    if (
+        os.getenv("GITHUB_ACTIONS")
+        or os.getenv("FORCE_COLOR")
+        or os.getenv("PY_COLORS")
+    ):
         return True
     return None
