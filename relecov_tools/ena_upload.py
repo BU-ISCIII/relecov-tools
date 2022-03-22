17ddf09e6ff2073fa5102d0ec3cb73f5d766b019
luissian <chapado.l@gmail.com>
2022-03-22 21:24:31 +0100

    working on the mapping to generate the data frames

--- relecov_tools/ena_upload.py
+++ relecov_tools/ena_upload.py
@@ -24,7 +24,7 @@ class EnaUpload:
         passwd=None,
         source_json=None,
         dev=None,
-        project=None,
+        customized_project=None,
         action=None,
         output_path=None,
     ):
@@ -52,10 +52,10 @@ class EnaUpload:
             )
         else:
             self.dev = dev
-        if project is None:
-            self.project = None
+        if customized_project is None:
+            self.customized_project = None
         else:
-            self.project = project
+            self.customized_project = customized_project
         if action is None:
             self.action = relecov_tools.utils.prompt_selection(
                 msg="Select the action to upload to ENA",
@@ -78,34 +78,48 @@ class EnaUpload:
             self.json_data = json.loads(fh.read())
 
     def convert_input_json_to_ena(self):
+        """Split the input ena json, in samples and runs json """
+        pass
+
+    def create_structure_to_ena(self):
         """Convert json to dataframe required by ena-upload-cli package"""
         schema_dataframe = {}
+
         config_json = ConfigJson()
-        map_to_upload = config_json.get_configuration("mapping_upload_ena")
+        map_to_upload = config_json.get_configuration("mapping_ena_to_upload_ena")
         map_file = os.path.join(
             os.path.dirname(os.path.realpath(__file__)), "schema", map_to_upload
         )
         fh = open(map_file)
-        map_json = json.load(fh)
+        map_structure_json = json.load(fh)
         fh.close()
 
-        for xml_file in map_json["xml_files"]:
+        for xml_file in map_structure_json["xml_files"]:
             if self.project is not None and xml_file in self.project:
                 pass
             elif config_json.get_configuration(xml_file):
                 df = pd.DataFrame.from_dict(
                     config_json.get_configuration(xml_file), orient="index"
                 )
+
             else:
-                pass
+                data = []
+                for chunk in self.json_data:
+                    xml_dict = {}
+                    for key, value in chunk.items():
+                        if key in map_structure_json[xml_file]:
+                            xml_dict[map_structure_json[xml_file][key]] = value
+                    data.append(xml_dict)
+                    if xml_file in map_structure_json["one_loop_in_mapping"]:
+                        break
             df = ena_upload.check_columns(df, xml_file, self.action, self.dev, False)
             schema_dataframe[xml_file] = df
         return schema_dataframe
 
     def upload_files_to_ena(self):
         """Create the required files and upload to ENA"""
-        # df = {}
         self.convert_input_json_to_ena()
+        self.create_structure_to_ena()
         # df_study = pd.DataFrame.from_dict(data["study"])
         # df_samples = pd.DataFrame.from_dict(data["samples"])
         # df_runs = pd.DataFrame.from_dict(data["runs"])
