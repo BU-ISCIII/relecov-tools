daa3d83be06d245dd519952dc5bdb850999e7331
luissian <chapado.l@gmail.com>
2022-03-22 21:22:45 +0100

    re-design the way to fetch the md5 files from dowloaded files

--- relecov_tools/utils.py
+++ relecov_tools/utils.py
@@ -3,6 +3,7 @@
 Common utility function for relecov_tools package.
 """
 import os
+import glob
 import hashlib
 from rich.console import Console
 import questionary
@@ -20,29 +21,48 @@ def file_exists(file_to_check):
     return False
 
 
-def get_md5_from_local_folder(local_folder, file_list):
+def get_md5_from_local_folder(local_folder):
     """Fetch the md5 values for each file in the file list"""
+    md5_results = {}
+    reg_for_md5 = os.path.join(local_folder, "*.md5")
+    reg_for_non_md5 = os.path.join(local_folder, "*[!.md5]")
+    md5_files = glob.glob(reg_for_md5)
+    if len(md5_files) > 0:
+        # find the md5 files and link them to the original file
+        non_md5_files = glob.glob(reg_for_non_md5)
+        for file_name in md5_files:
+            f_name, f_ext = os.path.splitext(file_name)
+            f_related_md5 = [v for v in non_md5_files if f_name in v]
+            if len(f_related_md5) == 1:
+                file_md5_path = os.path.join(local_folder, file_name)
+                fh = open(file_md5_path, "r")
+                md5_results[f_related_md5[0]] = fh.read()
+                fh.close()
+    return md5_results
+
+
+def calculate_md5(file_name):
+    """Calculate the md5 value for the file name"""
+    block_size = 2**20
+    return hashlib.md5(open(file_name, "rb").read(block_size)).hexdigest()
+
+
+def write_md5_file(file_name, md5_value):
+    """Write md5 to file """
+    with open(file_name, "wb") as fh:
+        fh.write(md5_value)
+    return
+
+
+def create_md5_files(file_list):
+    """Create the md5 files and return their value """
     md5_results = {}
     for file_name in file_list:
         f_name, f_ext = os.path.splitext(file_name)
-        if f_ext.lowercase() == ".md5":
-            file_md5_path = os.path.join(local_folder, file_name)
-            # file_origin_path = os
-            fh = open(file_path, "r")
-            md5_results[file_name]
-        if file_exists(file_path):
-            pass
-
-
-def calculate_md5(files_list):
-    """Calculate the md5 value for the list of files"""
-    block_size = 2**20
-    hash_md5 = hashlib.md5()
-    for file_list in files_list:
-        f_name, f_ext = os.path.splitext(file_list)
-        if not f_ext.lowercase() == ".md5":
-            hash_md5.update(open(file_list, "rb").read(block_size))
-    return hash_md5.hexdigest()
+        md5_results[os.path.base_name(f_name)] = calculate_md5(file_name)
+        md5_file_name = f_name + ".md5"
+        write_md5_file(md5_file_name, md5_results[os.path.base_name(f_name)])
+    return md5_results
 
 
 def save_local_md5(file_name, md5_value):
