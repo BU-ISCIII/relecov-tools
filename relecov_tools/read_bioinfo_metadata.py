815bae1f418b64d933ef21951e9703b93ccae43d
erika.kvalem <erika.kvalem@gmail.com>
2022-05-17 16:22:09 +0200

    finished v0 read_bioinfo_metadta

--- relecov_tools/read_bioinfo_metadata.py
+++ relecov_tools/read_bioinfo_metadata.py
@@ -4,12 +4,13 @@
 
 # from importlib.resources import path
 import logging
+from wsgiref.simple_server import software_version
 
 
 import rich.console
 from itertools import islice
 import pandas as pd
-
+import yaml
 
 import openpyxl
 import os
@@ -67,6 +68,10 @@ class BioinfoMetadata:
             "relecov_bioinfo_metadata"
         )
         c = 0
+        self.files_read_bioinfo_metadata = config_json.get_configuration(
+            "files_read_bioinfo_metadata"
+        )
+
         mapping_illumina_tab_path = os.path.join(
             self.input_folder, "mapping_illumina.tab"
         )
@@ -76,9 +81,31 @@ class BioinfoMetadata:
         variants_long_table_path = os.path.join(
             self.input_folder, "variants_long_table.csv"
         )
+        consensus_genome_length_path = os.path.join(
+            self.input_folder, "consensus_genome_length.csv"
+        )
+        software_versions_path = os.path.join(
+            self.input_folder, "software_versions.yml"
+        )
+        md5_info_path = os.path.join(
+            self.input_folder,
+            "md5sum_MiSeq_GEN_267_20220208_ICasas.md5",  # como hacer esto general para los servicios
+        )
+
         mapping_illumina_tab = pd.read_csv(mapping_illumina_tab_path, sep="\t")
         summary_variants_metrics = pd.read_csv(summary_variants_metrics_path, sep=",")
         variants_long_table = pd.read_csv(variants_long_table_path, sep=",")
+        consensus_genome_length = pd.read_csv(
+            consensus_genome_length_path, header=None, sep=","
+        )
+        md5_info = pd.read_csv(md5_info_path, header=None, sep=",")
+
+        with open(software_versions_path) as file:
+            software_versions = yaml.load(file, Loader=yaml.FullLoader)
+
+        self.mapping_illumina_tab_field_list = config_json.get_configuration(
+            "mapping_illumina_tab_field_list"
+        )
         for row in islice(ws_metadata_lab.values, 4, ws_metadata_lab.max_row):
             # row = ws_metadata_lab[5]
             sample_name = row[5]
@@ -94,55 +121,42 @@ class BioinfoMetadata:
             bioinfo_dict["consensus_sequence_filepath"] = self.input_folder
             bioinfo_dict["long_table_path"] = self.input_folder
             # fields from mapping_illumina.tab
-            bioinfo_dict["linage_name"] = mapping_illumina_tab["Lineage"][c]
-            bioinfo_dict["variant_designation"] = mapping_illumina_tab[
-                "Variantsinconsensusx10"
-            ][c]
-            bioinfo_dict["per_qc_filtered"] = mapping_illumina_tab["Coverage>10x(%)"][c]
-            # bioinfo_dict["per_reads_host"] = mapping_illumina_tab["%readshost"][c]
-            # bioinfo_dict["per_reads_virus"] = mapping_illumina_tab["%readsvirus"][c]
-            # bioinfo_dict["per_unmapped"] = mapping_illumina_tab["%unmapedreads"][c]
-            bioinfo_dict["per_Ns"] = mapping_illumina_tab["%Ns10x"][c]
-            bioinfo_dict["median_depth_of_coverage_value"] = mapping_illumina_tab[
-                "medianDPcoveragevirus"
-            ][c]
-            bioinfo_dict[
-                "number_of_variants_AF_greater_75percent"
-            ] = mapping_illumina_tab["Variantsinconsensusx10"][c]
-            bioinfo_dict["number_of_variants_with_effect"] = mapping_illumina_tab[
-                "MissenseVariants"
-            ][c]
+            for key in self.mapping_illumina_tab_field_list.keys():
+                bioinfo_dict[key] = mapping_illumina_tab[
+                    self.mapping_illumina_tab_field_list[key]
+                ][c]
             # fields from summary_variants_metrics_mqc.csv
             bioinfo_dict["number_of_base_pairs_sequenced"] = (
                 summary_variants_metrics["# Input reads"][c] * 2
-            )  # REVISAR SI ES ASÍ CON SARA
+            )
             bioinfo_dict["ns_per_100_kbp"] = summary_variants_metrics[
                 "# Ns per 100kb consensus"
             ][c]
-            # FALTA "consensus_genome_length": "", script que cuente el numero de nucleotidos tamaño del fasta
             # fields from variants_long_table.csv
             bioinfo_dict["reference_genome_accession"] = variants_long_table["CHROM"][c]
-            bioinfo_dict["consensus_sequence_name"] = str(sample_name).join(
-                ".consensus.fa"
-            )
-            c = +1
-            import pdb
+            bioinfo_dict["consensus_genome_length"] = consensus_genome_length.iloc[c, 0]
+            bioinfo_dict["consensus_sequence_name"] = md5_info.iloc[c, 0][0:32]
+            bioinfo_dict["consensus_sequence_name_md5"] = md5_info.iloc[c, 0][34:60]
 
-            pdb.set_trace()
-        """
-        f = open(path_illumina_tab, "r")
-        lines = f.readlines()
-        lineages = []
-        lineage_index = lines[0].index("Lineage")
-        for line in range(1, len(lines))
-        line_split = lines.split("\t")
-        lineages.append(line_split[lineage_index])
-        # "dehosting_method_software_version" # NO HARCODED
-        # "variant_calling_software_version" # NO HARCODED
-        # "consensus_sequence_software_version" # NO HARCODED
-        # "bioinformatics_protocol_software_version" # NO HARCODED
-        # "preprocessing_software_version"# NO HARCODED
-        # "mapping_software_version" # NO HARCODED
-        # bioinfo_dict["consensus_sequence_name"]=
-        # bioinfo_dict["consensus_sequence_name_md5"]=
-        """
+            bioinfo_dict["dehosting_method_software_version"] = list(
+                software_versions["KRAKEN2_KRAKEN2"].values()
+            )
+            bioinfo_dict["variant_calling_software_version"] = list(
+                software_versions["IVAR_VARIANTS"].values()
+            )
+            bioinfo_dict["consensus_sequence_software_version"] = list(
+                software_versions["BCFTOOLS_CONSENSUS"].values()
+            )
+
+            bioinfo_dict[
+                "bioinformatics_protocol_software_version"
+            ] = software_versions["Workflow"]["nf-core/viralrecon"]
+
+            bioinfo_dict["preprocessing_software_version"] = list(
+                software_versions["FASTP"].values()
+            )
+            bioinfo_dict["mapping_software_version"] = software_versions[
+                "BOWTIE2_ALIGN"
+            ].values()
+
+            c = +1
