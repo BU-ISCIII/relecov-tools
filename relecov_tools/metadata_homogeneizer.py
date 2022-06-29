30252c607e6d53c4de8a9f32e08085e1f7e3de0b
GuilleGorines <guillermo.gorines@gmail.com>
2022-06-29 13:02:57 +0200

    added detect_centre method

--- relecov_tools/metadata_homogeneizer.py
+++ relecov_tools/metadata_homogeneizer.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 # Imports
+import sys
 import json
 import pandas as pd
 
@@ -8,6 +9,7 @@ import pandas as pd
 class Homogeneizer:
     def __init__(filename, self):
         self.filename = filename
+        self.dictionary = None
         self.centre = None
         self.dataframe = None
         self.translated_dataframe = None
@@ -16,14 +18,34 @@ class Homogeneizer:
         return
 
     def detect_centre(self):
-        """Detect the origin centre of the metadata"""
+        """Detect the origin centre of the metadata, and finds the corresponding json file to use"""
+
+
         # Check name of the file attribute of the object
         # Check schema with all centres and find their json
         # associate centre and json with object
         # raise error when in doubt
         # must check on schema/institution_schemas
-        pass
+
+        path_to_institution_json = ""
+        detected = []
+        institution_dict = json.load(path_to_json)
+        
+        for key in institution_dict.keys():
+            if key in self.filename:
+                detected.append(institution_dict[key])
+        
+        if len(set(detected)) != 1:
+            print("some problems arised!!!") # change this to an elegant form
+            sys.exit() # maybe check which ones are being mixed or when none is being found
+        else:
+            print("works fine") # delete this after testing
+            self.dictionary = detected[0] # first item, they are all equal
+        
         return
+
+        
+
 
     def load_dataframe(self):
         """Read the metadata file"""
@@ -47,3 +69,4 @@ class Homogeneizer:
 
         pass
         return
+
