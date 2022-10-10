1dfc19ed77c933965d9a0abe1fd09e321ab952d9
Sara Monzón <sara.monzon.fdz@gmail.com>
2022-10-10 16:59:43 +0200

    delete test code

--- relecov_tools/read_lab_metadata.py
+++ relecov_tools/read_lab_metadata.py
@@ -153,19 +153,7 @@ class RelecovMetadata:
 
     def process_from_json(self, m_data, json_fields):
         """ """
-#         if isinstance(json_fields["map_field"], dict):
-#             # Search for the value which contains data
-#             for m_field in json_fields["map_field"]["any_of"]:
-#                 try:
-#                     #m_data[0][m_field]
-#                     map_field = m_field
-#                 except KeyError:
-#                     continue
-#        else:
-
         map_field = json_fields["map_field"]
-
-        import pdb; pdb.set_trace()
 
         json_data = json_fields["j_data"]
 
