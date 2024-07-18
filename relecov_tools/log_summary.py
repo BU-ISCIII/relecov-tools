6811e4e8a2b2440dd4b0e693c10ee56efc64f9e5
Shettland <pyblus@gmail.com>
2024-07-18 16:45:19 +0200

    Updated log_summary main keys and new path field

--- relecov_tools/log_summary.py
+++ relecov_tools/log_summary.py
@@ -3,6 +3,8 @@ import logging
 import json
 import os
 import inspect
+import sys
+import copy
 from rich.console import Console
 from datetime import datetime
 from collections import OrderedDict
@@ -21,36 +23,61 @@ stderr = Console(
 
 
 class LogSum:
-    def __init__(self, output_location: str = None, only_samples: bool = False):
+    def __init__(
+        self,
+        output_location: str = None,
+        only_samples: bool = False,
+        unique_key: str = None,
+        path: str = None,
+    ):
         if not os.path.exists(output_location):
             raise FileNotFoundError("Output location does not exist")
         else:
             self.output_location = output_location
+        if only_samples and unique_key:
+            stderr.print("[red]LogSum only_samples and unique_key are incompatible")
+            sys.exit(1)
         # if only_samples is given, no "samples" key will be added to logs
         if only_samples:
             self.only_samples = True
         else:
             self.only_samples = False
+        # if unique_key is given, all entries will be saved inside that key by default
+        if unique_key:
+            self.unique_key = unique_key
+        else:
+            self.unique_key = None
+        # if path is given, all keys will include a field "path" with this value
+        if path:
+            self.path = path
+        else:
+            self.path = None
         self.logs = {}
         return
 
-    def feed_key(self, key, sample=None):
+    def feed_key(self, key=None, sample=None):
         """Run update_summary() with no entry nor log_type. Add a new empty key"""
+        if self.unique_key:
+            key = self.unique_key
         self.update_summary(log_type=None, key=key, entry=None, sample=sample)
 
-    def add_error(self, key, entry, sample=None):
+    def add_error(self, entry, key=None, sample=None):
         """Run update_summary() with log_type as errors"""
+        if self.unique_key:
+            key = self.unique_key
         log.error(entry)
         self.update_summary(log_type="errors", key=key, entry=entry, sample=sample)
         return
 
-    def add_warning(self, key, entry, sample=None):
+    def add_warning(self, entry, key=None, sample=None):
         """Run update_summary() with log_type as warnings"""
+        if self.unique_key:
+            key = self.unique_key
         log.warning(entry)
         self.update_summary(log_type="warnings", key=key, entry=entry, sample=sample)
         return
 
-    def update_summary(self, key, log_type, entry, sample=None):
+    def update_summary(self, log_type, key, entry, sample=None):
         """Create a dictionary with a defined structure for each new key. Add the
         entry to the dictionary if it already exists. Add it to samples if its a sample
 
@@ -72,18 +99,20 @@ class LogSum:
             sample = None
         entry, sample = (str(entry), str(sample))
         if current_key not in self.logs.keys():
-            self.logs[current_key] = feed_dict.copy()
+            self.logs[current_key] = copy.deepcopy(feed_dict)
+            if self.path and "path" not in self.logs[current_key]:
+                self.logs[current_key].update({"path": self.path})
             if not self.only_samples:
                 self.logs[current_key]["samples"] = OrderedDict()
         if log_type is None:
             if sample != "None" and sample not in self.logs[current_key]["samples"]:
-                self.logs[current_key]["samples"][sample] = feed_dict.copy()
+                self.logs[current_key]["samples"][sample] = copy.deepcopy(feed_dict)
             return
         if sample == "None":
             self.logs[current_key][log_type].append(entry)
         else:
             if sample not in self.logs[current_key]["samples"].keys():
-                self.logs[current_key]["samples"][sample] = feed_dict.copy()
+                self.logs[current_key]["samples"][sample] = copy.deepcopy(feed_dict)
             self.logs[current_key]["samples"][sample][log_type].append(entry)
         return
 
@@ -113,6 +142,8 @@ class LogSum:
             filename = "_".join([date, called_module, "log_summary.json"])
         summary_path = os.path.join(self.output_location, filename)
         with open(summary_path, "w", encoding="utf-8") as f:
-            f.write(json.dumps(self.logs, indent=4, sort_keys=True, ensure_ascii=False))
-        stderr.print(f"Process log summary printed in {summary_path}")
+            f.write(
+                json.dumps(self.logs, indent=4, sort_keys=False, ensure_ascii=False)
+            )
+        stderr.print(f"Process log summary saved in {summary_path}")
         return
