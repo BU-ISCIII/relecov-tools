20c81b613b61132d5cc63cebb874e4fb62a07db9
svarona <sarai.var.fer@gmail.com>
2024-12-30 17:55:23 +0100

    Added dev to version to keep in logfiles0

--- relecov_tools/__main__.py
+++ relecov_tools/__main__.py
@@ -35,7 +35,7 @@ stderr = rich.console.Console(
     stderr=True, force_terminal=relecov_tools.utils.rich_force_colors()
 )
 
-__version__ = "1.3.0"
+__version__ = "1.3.0dev"
 
 
 def run_relecov_tools():
