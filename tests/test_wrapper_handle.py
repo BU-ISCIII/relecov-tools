c0ae9e918dd637cbb8abac9346d9145426bc6462
Alejandro <aberdur7@gmail.com>
2025-02-26 15:24:04 +0100

    Update test_Wrapper

--- tests/test_wrapper_handle.py
+++ tests/test_wrapper_handle.py
@@ -119,7 +119,7 @@ def prepare_remote_test(**kwargs):
         output_folder=kwargs["output_location"],
     )
 
-    print("Update Wrapper param")
+    print("Update Wrapper params")
     wrapper_manager.config_data["download"]["user"] = kwargs["user"]
     wrapper_manager.config_data["download"]["passwd"] = kwargs["password"]
     wrapper_manager.config_data["download"]["download_option"] = kwargs[
