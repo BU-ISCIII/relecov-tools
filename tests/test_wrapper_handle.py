ec072a7c849813449719f0ee56d0f32a0680c7d8
Alejandro <aberdur7@gmail.com>
2025-02-26 16:19:38 +0100

    Assign download parameters to self.download_params

--- tests/test_wrapper_handle.py
+++ tests/test_wrapper_handle.py
@@ -107,8 +107,8 @@ def prepare_remote_test(**kwargs):
 
     print("Initiating wrapper configuration")
     conf_file = generate_config_yaml(
-        kwargs["user"],
-        kwargs["password"],
+        "user",
+        "password",
         kwargs["download_option"],
         kwargs["target_folders"],
     )
@@ -120,42 +120,15 @@ def prepare_remote_test(**kwargs):
     )
 
     print("Update Wrapper params")
-    wrapper_manager.config_data["download"]["user"] = kwargs["user"]
-    wrapper_manager.config_data["download"]["passwd"] = kwargs["password"]
-    wrapper_manager.config_data["download"]["download_option"] = kwargs[
-        "download_option"
-    ]
-    wrapper_manager.config_data["download"]["output_location"] = kwargs[
-        "output_location"
-    ]
-    wrapper_manager.config_data["download"]["target_folders"] = kwargs["target_folders"]
+    wrapper_manager.download_params = {
+        "user": kwargs["user"],
+        "passwd": kwargs["password"],
+        "download_option": kwargs["download_option"],
+        "output_location": kwargs["output_location"],
+        "target_folders": kwargs["target_folders"],
+    }
 
-    wrapper_manager.config_data["read-lab-metadata"][
-        "metadata_file"
-    ] = "tests/data/read_lab_metadata/metadata_lab_test.xlsx"
-    wrapper_manager.config_data["read-lab-metadata"][
-        "sample_list_file"
-    ] = "tests/data/read_lab_metadata/samples_data_test.json"
-
-    wrapper_manager.config_data["validate"][
-        "json_schema_file"
-    ] = "relecov_tools/schema/relecov_schema.json"
-
-    wrapper_manager.download_params = wrapper_manager.clean_module_params(
-        "DownloadManager", wrapper_manager.config_data["download"]
-    )
-    wrapper_manager.readmeta_params = wrapper_manager.clean_module_params(
-        "RelecovMetadata", wrapper_manager.config_data["read-lab-metadata"]
-    )
-    wrapper_manager.validate_params = wrapper_manager.clean_module_params(
-        "SchemaValidation", wrapper_manager.config_data["validate"]
-    )
-
-    def test_wrapper(wrapper_manager):
-        wrapper_manager.run_wrapper()
-
-    test_wrapper(wrapper_manager)
-
+    wrapper_manager.run_wrapper()
 
 if __name__ == "__main__":
     main()
