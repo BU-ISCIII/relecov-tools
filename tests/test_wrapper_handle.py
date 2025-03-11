a0d5e6b88e24e1d1b9d7cfd522c1e7fc4f05e3c0
Alejandro <aberdur7@gmail.com>
2025-03-11 10:00:29 +0100

    Remove port assignation from wrapper_manager.download_manager

--- tests/test_wrapper_handle.py
+++ tests/test_wrapper_handle.py
@@ -130,15 +130,6 @@ def prepare_remote_test(**kwargs):
 
     # wrapper_manager.download_manager.relecov_sftp.sftp_port = os.environ["TEST_PORT"]
 
-    if wrapper_manager.download_manager is not None:
-        wrapper_manager.download_manager.relecov_sftp.sftp_port = os.environ[
-            "TEST_PORT"
-        ]
-    else:
-        print(
-            "Warning: `download_manager` is not initialized yet. Skipping SFTP port assignment."
-        )
-
     def test_wrapper(wrapper_manager):
         wrapper_manager.run_wrapper()
 
