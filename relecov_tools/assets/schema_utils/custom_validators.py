1962c124b14379430ebb75a060305854a9655772
Daniel-VM <danielvmillares@gmail.com>
2025-03-11 17:32:08 +0100

    added custom validator to extend validation exceptions

---
+++ relecov_tools/assets/schema_utils/custom_validators.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+
+
+def validate_with_exceptions(schema, data, errors):
+    """Filter out type errors for:
+    - integer/float fields containing 'Not Provided'
+    - string fields with format: date containing 'Not Provided'"""
+    filtered_errors = []
+
+    for error in errors:
+        property_path = ".".join(str(p) for p in error.path)
+        prop_schema = schema["properties"].get(property_path, {})
+
+        # allow not provided for numeric types
+        if (
+            error.validator == "type"
+            and error.instance == "Not Provided [GENEPIO:0001668]"
+            and prop_schema.get("type") in ["integer", "number"]
+        ):
+            continue
+
+        # allow not provided for date format types
+        if (
+            error.validator == "format"
+            and error.instance == "Not Provided [GENEPIO:0001668]"
+            and prop_schema.get("type") == "string"
+            and prop_schema.get("format") == "date"
+        ):
+            continue
+
+        # Keep all other errors
+        filtered_errors.append(error)
+
+    return
