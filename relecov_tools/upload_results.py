54477cbfd8aa078dfadbe7ff9da0a0dd47c03da3
victor5lm <victor_lopez_5@hotmail.com>
2025-03-17 12:09:23 +0100

    Updated upload_results.py to send email

--- relecov_tools/upload_results.py
+++ relecov_tools/upload_results.py
@@ -7,6 +7,7 @@ import json
 import paramiko
 import relecov_tools.utils
 import relecov_tools.sftp_client
+import relecov_tools.mail
 from secrets import token_hex
 from datetime import datetime
 from rich.console import Console
@@ -15,9 +16,8 @@ from relecov_tools.config_json import ConfigJson
 log = logging.getLogger(__name__)
 stderr = Console(stderr=True, style="dim", highlight=False, force_terminal=relecov_tools.utils.rich_force_colors())
 
-
 class UploadSftp:
-    def __init__(self, user=None, passwd=None, batch_id=None):
+    def __init__(self, user=None, passwd=None, batch_id=None, template_path=None):
         """Starts the SFTP upload process"""
         log.info(f"Beginning upload process for batch: {batch_id}")
 
@@ -25,7 +25,12 @@ class UploadSftp:
             stderr.print("[red]Error: You must provide a batch_id as an argument.")
             sys.exit(1)
 
+        if not template_path:
+            stderr.print("[red]Error: You must provide a template_path as an argument.")
+            sys.exit(1)
+
         config_json = ConfigJson()
+        config = config_json.get_configuration("mail_sender")
         self.allowed_file_ext = config_json.get_topic_data(
             "sftp_handle", "allowed_file_extensions"
         )
@@ -41,10 +46,13 @@ class UploadSftp:
         stderr.print(f"User: {self.sftp_user}, Processing batch: {self.batch_id}")
 
         self.processed_batches = {}  # Dictionary to store results
+        self.template_path = template_path
+        self.email_sender = relecov_tools.mail.EmailSender(config, template_path)
+        self.guide = config.get("institutions_guide_path")
 
     def find_cod_for_batch(self):
         """Find all COD-* folders containing the batch_id"""
-        base_dir = os.getcwd() #This module should be run in root directory
+        base_dir = os.getcwd()  # This module should be run in root directory
         cod_dirs = [d for d in os.listdir(base_dir) if os.path.isdir(d) and d.startswith("COD-")]
 
         matching_cod = {}
@@ -75,7 +83,7 @@ class UploadSftp:
 
         password = token_hex(8).encode()  # Creates a random password
         zip_filename = f"{batch}_results_{datetime.now().strftime('%Y%m%d%H%M%S')}.zip"
-        zip_path = os.path.join(os.getcwd(), zip_filename)
+        zip_path = os.path.join(batch_path, zip_filename)
 
         with pyzipper.AESZipFile(zip_path, 'w', compression=pyzipper.ZIP_DEFLATED, encryption=pyzipper.WZ_AES) as zipf:
             zipf.setpassword(password)  # The password is assigned to ZIP file
@@ -83,6 +91,8 @@ class UploadSftp:
                 for file in files:
                     file_path = os.path.join(root, file)
                     arcname = os.path.relpath(file_path, analysis_dir)
+                    if arcname.startswith('.'):
+                        arcname = arcname[1:]  # Remove leading dot if present
                     zipf.write(file_path, arcname)
 
         stderr.print(f"[green]Compressed file: {zip_path} with password: {password.decode()}")
@@ -94,7 +104,7 @@ class UploadSftp:
             stderr.print("[red]Could not connect to SFTP server")
             return False
 
-        sftp = self.relecov_sftp.sftp  # Obtener el objeto SFTP
+        sftp = self.relecov_sftp.sftp
 
         # Define the remote path where the file is to be uploaded
         remote_dir = f"/{cod}/ANALYSIS_RESULTS"
@@ -127,9 +137,37 @@ class UploadSftp:
         finally:
             self.relecov_sftp.close_connection()
 
+    def notify_lab(self, batch_name, lab_code, receiver_email, email_psswd=None, zip_passwd=None, zip_filename=None):
+        """
+        Sends an email notification to the laboratory with results details.
+        """
+        # Render the email content using the Jinja2 template
+        email_body = self.email_sender.render_email_template(
+            additional_info=f"Batch {batch_name} is ready.",
+            submitting_institution_code=lab_code,
+            template_name="jinja_template_results.j2",
+            batch=batch_name,
+            password=zip_passwd,
+            zip_filename=zip_filename
+        )
+
+        # Send the email
+        self.email_sender.send_email(
+            receiver_email=receiver_email,
+            subject=f"Batch {batch_name} - Resultados del análisis",
+            body=email_body,
+            attachments=[],
+            email_psswd=email_psswd
+        )
+
+        print(f"Notification sent to {receiver_email} for batch {batch_name}.")
+
     def execute_process(self):
-        """Runs the complete flow: search, compress, upload and logging"""
+        """Runs the complete flow: search, compress, upload, logging and sending the email."""
         cod_batches = self.find_cod_for_batch()
+
+        with open(self.guide, 'r') as f:
+            institutions_data = json.load(f)
 
         for cod, batch_data in cod_batches.items():
             stderr.print(f"Processing batch {batch_data['batch']} in {cod}")
@@ -149,3 +187,11 @@ class UploadSftp:
                     })
 
                     stderr.print(f"[green]Process completed for {batch_data['batch']} in {cod}.")
+
+                    institution_info = institutions_data.get(cod)
+
+                    if institution_info:
+                    # After successful upload, send the results email
+                        receiver_email = [institution_info["email_receiver"]]
+                        lab_code = cod
+                        self.notify_lab(batch_data['batch'], lab_code, receiver_email, zip_passwd=password, zip_filename=zip_filename)
