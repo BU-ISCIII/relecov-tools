0c13d2fdf3e16e5022673106760472d80215226d
Alejandro <aberdur7@gmail.com>
2025-04-03 12:03:44 +0200

    Improve read-bioinfo-metadata modularity

--- relecov_tools/assets/pipeline_utils/viralrecon.py
+++ relecov_tools/assets/pipeline_utils/viralrecon.py
@@ -467,35 +467,25 @@ def quality_control_evaluation(data):
     log_report = BioinfoReportLog()
     method_name = f"{quality_control_evaluation.__name__}"
 
-    def safe_float(x):
+    def is_number(x):
         try:
-            return float(x)
+            float(x)
+            return True
         except (ValueError, TypeError):
-            return None
-
-    def safe_int(x):
-        try:
-            return int(x)
-        except (ValueError, TypeError):
-            return None
+            return False
 
     conditions = {
-        "per_sgene_ambiguous": lambda x: safe_float(x) is not None
-        and safe_float(x) < 10,
-        "per_sgene_coverage": lambda x: safe_float(x) is not None
-        and safe_float(x) > 98,
+        "per_sgene_ambiguous": lambda x: is_number(x) and float(x) < 10,
+        "per_sgene_coverage": lambda x: is_number(x) and float(x) > 98,
         "per_ldmutations": lambda x: (
-            True
-            if isinstance(x, str) and "Not Evaluable" in x
-            else safe_float(x) is not None and safe_float(x) > 60
+            True if isinstance(x, str) and "Not Evaluable" in x
+            else is_number(x) and float(x) > 60
         ),
-        "number_of_sgene_frameshifts": lambda x: safe_int(x) is not None
-        and safe_int(x) == 0,
-        "number_of_unambiguous_bases": lambda x: safe_int(x) is not None
-        and safe_int(x) > 24000,
-        "number_of_Ns": lambda x: safe_int(x) is not None and safe_int(x) < 5000,
-        "qc_filtered": lambda x: safe_int(x) is not None and safe_int(x) > 50000,
-        "per_reads_host": lambda x: safe_float(x) is not None and safe_float(x) < 20,
+        "number_of_sgene_frameshifts": lambda x: is_number(x) and int(x) == 0,
+        "number_of_unambiguous_bases": lambda x: is_number(x) and int(x) > 24000,
+        "number_of_Ns": lambda x: is_number(x) and int(x) < 5000,
+        "qc_filtered": lambda x: is_number(x) and int(x) > 50000,
+        "per_reads_host": lambda x: is_number(x) and float(x) < 20,
     }
 
     for sample in data:
