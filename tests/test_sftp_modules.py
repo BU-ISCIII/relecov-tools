8fb2edcabbd3be2ef41a81b349f57d19a6aa44b0
Alejandro <aberdur7@gmail.com>
2025-04-28 11:01:52 +0200

    Add time control to download test

--- tests/test_sftp_modules.py
+++ tests/test_sftp_modules.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python
 import os
 import sys
+import time
 import argparse
 from relecov_tools.download_manager import DownloadManager
-
 
 def main():
     parser = argparse.ArgumentParser()
@@ -25,9 +25,27 @@ def main():
     }
     prepare_remote_test(**val_dict)
 
+def wait_until_files_exist(sftp_client, folders, timeout=30):
+    """Wait until folders exist and are non-empty in SFTP"""
+    start_time = time.time()
+    while time.time() - start_time < timeout:
+        ready = True
+        for folder in folders:
+            try:
+                files = sftp_client.get_file_list(folder)
+                if not files:
+                    ready = False
+                    break
+            except Exception:
+                ready = False
+                break
+        if ready:
+            return True
+        time.sleep(1)
+    raise TimeoutError(f"Timeout waiting for folders: {folders}")
 
 def prepare_remote_test(**kwargs):
-    print("Initating sftp module")
+    print("Initiating sftp module")
     download_manager = DownloadManager(
         user=kwargs["user"],
         passwd=kwargs["password"],
@@ -37,7 +55,7 @@ def prepare_remote_test(**kwargs):
         target_folders=kwargs["target_folders"],
     )
 
-    print("Openning connection to sftp")
+    print("Opening connection to sftp")
     download_manager.relecov_sftp.sftp_port = os.environ["TEST_PORT"]
     if not download_manager.relecov_sftp.open_connection():
         print("Could not open connection to remote sftp")
@@ -75,6 +93,7 @@ def prepare_remote_test(**kwargs):
     data_loc = "tests/data/sftp_handle"
     folder_files_dict = {folder: files for folder, _, files in os.walk(data_loc)}
     print("Uploading files to sftp...")
+    uploaded_folders = []
     for folder, files in folder_files_dict.items():
         if "datatest" in folder:
             remote_dir = "COD-test-1/RELECOV"
@@ -83,20 +102,25 @@ def prepare_remote_test(**kwargs):
         else:
             continue
         base_folder = folder.split("/")[-1]
-        download_manager.relecov_sftp.make_dir(os.path.join(remote_dir, base_folder))
+        remote_folder_path = os.path.join(remote_dir, base_folder)
+        download_manager.relecov_sftp.make_dir(remote_folder_path)
+        uploaded_folders.append(remote_folder_path)
         print(f"Uploading files from {base_folder}")
         for file in files:
-            remote_path = os.path.join(remote_dir, base_folder, file)
+            remote_path = os.path.join(remote_folder_path, file)
             local_path = os.path.join(os.path.abspath(folder), file)
             download_manager.relecov_sftp.upload_file(local_path, remote_path)
+
+    print("Waiting for files to be ready in SFTP...")
+    wait_until_files_exist(download_manager.relecov_sftp, uploaded_folders)
 
     download_manager.relecov_sftp.close_connection()
 
     def test_download(download_manager):
+        print("Starting download process...")
         download_manager.execute_process()
 
     test_download(download_manager)
-
 
 if __name__ == "__main__":
     main()
