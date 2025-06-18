0b03e521606a47d095486127257b6a688cc70b9e
Sara Monzón <sara.monzon.fdz@gmail.com>
2025-06-18 14:02:48 +0200

    lint

--- relecov_tools/download_manager.py
+++ relecov_tools/download_manager.py
@@ -505,9 +505,7 @@ class DownloadManager(BaseModule):
                     self.include_error(entry=str(log_text % s_name), sample=s_name)
                 try:
                     if not row[index_layout]:
-                        error_text = (
-                            "Missing 'Library Layout' value for sample %s."
-                        )
+                        error_text = "Missing 'Library Layout' value for sample %s."
                         self.include_error(error_text % str(sample_id), s_name)
                     if (
                         "paired" in row[index_layout].lower()
